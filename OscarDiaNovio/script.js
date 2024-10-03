16fe0d70200b1b84d1924d7ad9ea44b33263f568
Casttmari12 <castromariana910@gmail.com>
2024-10-02 22:55:06 -0600

    Delete OscarDiaNovio directory

--- OscarDiaNovio/script.js
+++
@@ -1,8 +0,0 @@
-// Puedes añadir alguna interacción divertida aquí si deseas
-console.log("Página creada con amor para Oscar 💖");
-document.getElementById("couponButton").addEventListener("click", function() {
-    this.textContent = "Cupón";
-    this.style.backgroundColor = "#32cd32"; // Cambia a verde
-    this.style.color = "white";
-    document.getElementById("coupons").classList.remove("hidden");
-});
